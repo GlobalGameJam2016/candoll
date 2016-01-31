4c5f195f08c8c210678cb0b0306bb71bc9788e7d
Daniel Choi <cys920622@gmail.com>
2016-01-30 21:11:28 -0800

    Implement basic chaser AI, add dog

---
+++ Candoll/Assets/Scripts/AI1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI1 : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+}
