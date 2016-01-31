f18487f66ce0b7d83565ef3544ee8f3e2dc45fef
Trent Jeffery <trentjeffery@shaw.ca>
2016-01-30 19:37:21 -0800

    Enemy FOV and detection added

---
+++ candoll/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Actor {
+
+    int viewDist;
+    int viewAngle;
+    Vector2 lookat;
+
+
+	// Use this for initialization
+	void Start () {
+        viewDist = 5;
+        viewAngle = 30;
+	}
+	
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
