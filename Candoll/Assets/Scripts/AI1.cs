64c3105498c622eeeb7428dc539a05f7d520d74b
KChan <chan.kelv@gmail.com>
2016-01-31 03:56:27 -0800

    Dog

--- Candoll/Assets/Scripts/AI1.cs
+++ Candoll/Assets/Scripts/AI1.cs
@@ -7,6 +7,13 @@ public class AI1 : MonoBehaviour {
 	public float speed;
 	public float chaseRadius;
 	private float distance;
+	Animator anim;
+
+	void Start () {
+		//gets animator object
+		anim = GetComponent<Animator>();
+		anim.SetInteger ("Direction", 0);
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -14,7 +21,15 @@ public class AI1 : MonoBehaviour {
 		if (distance < chaseRadius) {
 			float step = speed * Time.deltaTime;
 			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+
+			//animate dog
+			if (transform.position.x > target.position.x)
+				anim.SetInteger ("Direction", 3);
+			else
+				anim.SetInteger ("Direction", 1);
 		}
+
+//		anim.SetInteger ("Direction", 0);
 	}
 //
 //	void OnCollisionEnter2D (Collision2D obj)
